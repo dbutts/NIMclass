bf53e93cd687a466514a8ee20e516cd6616d3923
Dan Butts <dbutts@Peabrain.home>
2016-02-09 10:11:33 -0500

    Bug fix in subunit filter-display
    — also allowed non-cell Xstim to be passed in

--- SUBUNIT.m
+++ SUBUNIT.m
@@ -244,7 +244,7 @@ methods
 				subunit.display_spatial_filter( dims, modvarargin{:} );
 			else
 				k = reshape( subunit.get_filtK(), dims(1:2) );
-				imagesc( 1:dims(1),1:dims(2), k, Kmax*[-1 1] )
+				imagesc( 1:dims(1),1:dims(2), k, max(abs(k(:)))*[-1 1] )
 				if isfield(parsed_options,'colormap')
 					colormap(parsed_options.colormap);
 				else
